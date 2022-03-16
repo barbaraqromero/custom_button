55c582b33f72430a0536182c8464b8b12675e032
Bárbara Romero <bqromero@gmail.com>
2022-03-16 18:22:45 -0300

    Finalizar componente customizado

--- app/src/main/java/br/com/zup/custombutton/MainActivity.kt
+++ app/src/main/java/br/com/zup/custombutton/MainActivity.kt
@@ -2,10 +2,16 @@ package br.com.zup.custombutton
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.zup.custombutton.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.progressButton2.setOnClickListener{
+            binding.progressButton2.setLoading()
+        }
     }
 }
